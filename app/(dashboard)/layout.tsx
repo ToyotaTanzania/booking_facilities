39484052c228ad299f7a25db368198e3b0e1367a
Feisal Ali <feisalramar@gmail.com>
2025-09-18 16:17:01 +0300

    Add SignOutButton to DashboardLayout for user logout functionality
    - Integrated `SignOutButton` into the `DashboardLayout` to allow users to log out easily.
    - Positioned the button in the top-right corner for better accessibility and visibility.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { SignOutButton } from "../auth/signin/signout";
 import { Sidebar } from "./_components/sidebar";
 import CheckAuth from "./check-auth";
 
@@ -9,6 +10,9 @@ export default function DashboardLayout({
   return (
     <div className="relative flex min-h-screen">
       {/* <Sidebar /> */}
+      <div className="absolute top-4 right-4 z-50"> 
+        <SignOutButton />
+      </div>
       <main className="flex-1 overflow-y-auto bg-muted/10 pb-16">
         <div className="flex-1 space-y-4 p-2">
           <CheckAuth /> 
